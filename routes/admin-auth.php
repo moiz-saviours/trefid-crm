c932cbdc7b84c0b1707992978582990d9c876846
arhumsaviours <arhum@saviours.co>
2024-11-02 03:25:26 +0500

    space

--- routes/admin-auth.php
+++ routes/admin-auth.php
@@ -36,5 +36,8 @@ Route::prefix('admin')->name('admin.')->group(function () {
 //        Route::post('confirm-password', [AdminConfirmablePasswordController::class, 'store']);
 //        Route::put('password', [AdminPasswordController::class, 'update'])->name('password.update');
         Route::post('logout', [AdminAuthenticatedSessionController::class, 'destroy'])->name('logout');
+
+
+        
     });
 });
