341483da8d0178295052e8ce760982d6f7171bdd
moiz-saviours <moiz@saviours.co>
2025-01-09 00:28:27 +0500

    Update index.blade.php

--- resources/views/admin/customers/companies/index.blade.php
+++ resources/views/admin/customers/companies/index.blade.php
@@ -31,7 +31,7 @@
                 <div class="container">
                     <div class="custom-tabs">
                         <ul class="tab-nav">
-                            <li class="tab-item active" data-tab="home">Employees
+                            <li class="tab-item active" data-tab="home">Companies
                                 <i class="fa fa-times close-icon" aria-hidden="true"></i></li>
                         </ul>
                     </div>
