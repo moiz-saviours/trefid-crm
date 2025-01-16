91c7c8c405a634b4e5eb81761cffb9f22eaeb431
moiz-saviours <moiz@saviours.co>
2025-01-16 23:12:12 +0500

    Update ActivityLoggable.php

--- app/Traits/ActivityLoggable.php
+++ app/Traits/ActivityLoggable.php
@@ -13,27 +13,19 @@ trait ActivityLoggable
     protected static function bootActivityLoggable()
     {
         static::created(function ($model) {
-            $model->logActivity('create', auth()->user()->name . " created a new " . ($model->getTable()) . ": " . $model->name);
+            $model->logActivity('create', auth()->user()->name??null . " created a new " . ($model->getTable()) . ": " . $model->name);
         });
-
         static::updated(function ($model) {
             $entityName = $model->name ?? 'Unknown';
-
-            $model->logActivity('updated', auth()->user()->name . " updated " . ($model->getTable()) . ": " . $entityName);
+            $model->logActivity('updated', auth()->user()->name??null . " updated " . ($model->getTable()) . ": " . $entityName);
         });
-
-
         static::deleted(function ($model) {
             $entityName = $model->name ?? 'Unknown';
-
             // Log the activity
-            $model->logActivity('deleted', auth()->user()->name . " deleted " . ($model->getTable()) . ": " . $entityName);
+            $model->logActivity('deleted', auth()->user()->name??null . " deleted " . ($model->getTable()) . ": " . $entityName);
         });
 
-
     }
-
-
 
     /**
      * Log the activity for the model.
@@ -44,15 +36,18 @@ trait ActivityLoggable
      */
     protected function logActivity($action, $description)
     {
-        ActivityLog::create([
-            'action' => $action,
-            'model_type' => get_class($this),
-            'model_id' => $this->id,
-            'actor_type' => get_class(auth()->user()),
-            'actor_id' => auth()->id(),
-            'description' => $description,
-            'ip_address' => request()->ip(),
-        ]);
+        if (auth()->check()) {
+
+            ActivityLog::create([
+                'action' => $action,
+                'model_type' => get_class($this),
+                'model_id' => $this->id,
+                'actor_type' => get_class(auth()->user()),
+                'actor_id' => auth()->id(),
+                'description' => $description,
+                'ip_address' => request()->ip(),
+            ]);
+        }
     }
 }
 
