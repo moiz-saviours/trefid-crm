63c1931c349f3e4bac2a858d95cd7a3ed50b1724
Hk2592755107 <hussain.khan@saviours.co>
2025-02-04 19:31:21 +0500

    Update All Modules in Admin Side

--- resources/views/admin/brands/script.blade.php
+++ resources/views/admin/brands/script.blade.php
@@ -143,7 +143,9 @@
             return decodeHtmlEntities(data);
         };
 
-        const exportButtons = ['copy', 'excel', 'csv', 'pdf', 'print'].map(type => ({
+        const exportButtons = ['copy', 'excel', 'csv'
+            //, 'pdf'
+            , 'print'].map(type => ({
             extend: type,
             text: type == "copy"
                 ? '<i class="fas fa-copy"></i>'
