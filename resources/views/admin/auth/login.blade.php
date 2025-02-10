ff570c97632173e370c7794ef03dbdf96ff40710
Hk2592755107 <hussain.khan@saviours.co>
2025-02-10 15:16:37 +0500

    update user side and admin side links

--- resources/views/admin/auth/login.blade.php
+++ resources/views/admin/auth/login.blade.php
@@ -10,8 +10,7 @@
     <title>{{ config('app.name', 'Laravel') }}</title>
     <link rel="stylesheet" href="./style.css/">
     <link rel="stylesheet" href="{{asset('build/toaster/css/toastr.min.css')}}">
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet"
-          integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <link rel="stylesheet" href="{{asset ('assets/css/bootstrap.min.css')}}">
 
     @include('admin.auth.style')
 
