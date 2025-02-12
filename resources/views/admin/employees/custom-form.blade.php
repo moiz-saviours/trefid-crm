264df24aeb3b175fbe06f30e3c2d2720ae2b9827
moiz-saviours <moiz@saviours.co>
2025-02-12 17:01:19 +0500

    Update custom-form.blade.php

--- resources/views/admin/employees/custom-form.blade.php
+++ resources/views/admin/employees/custom-form.blade.php
@@ -27,11 +27,9 @@
 @endpush
 <div class="custom-form">
     <form id="manage-form" class="manage-form" method="POST" enctype="multipart/form-data">
-        <label for="crsf_token" class="form-label d-none">Crsf Token</label>
-        <input type="text" id="crsf_token" name="crsf_token" value="" style="opacity:0;position:absolute;"/>
-        <div class="form-container" id="formContainer"><div class="form-container" id="formContainer">
-                <label for="crsf_token" class="form-label d-none">Crsf Token</label>
-                <input type="text" id="crsf_token" name="crsf_token" value="" style="opacity:0;position:absolute;"/>
+        <div class="form-container" id="formContainer">
+            <label for="crsf_token" class="form-label d-none">Crsf Token</label>
+            <input type="text" id="crsf_token" name="crsf_token" value="" style="opacity:0;position:absolute;"/>
             <!-- Form Header -->
             <div class="form-header fh-1">
                 <span id="custom-form-heading">Manage Employee</span>
