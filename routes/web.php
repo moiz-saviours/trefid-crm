806706d37cddb32fe962ba6b85f413c442d908fb
moiz-saviours <moiz@saviours.co>
2025-02-13 20:02:33 +0500

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -22,7 +22,7 @@ use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return route('login')
+    return route('login');
 //    return view('welcome');
 });
 require __DIR__ . '/auth.php';
