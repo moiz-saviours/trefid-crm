3840a0bcf927861ca7d664b0051e0560c2d0f3f5
moiz-saviours <moiz@saviours.co>
2025-02-19 20:06:55 +0500

    Update checkout.blade.php

--- resources/views/checkout.blade.php
+++ resources/views/checkout.blade.php
@@ -4,8 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta name="csrf-token" content="{{ csrf_token() }}">
-
-    <title>Document</title>
+    <title>Invoice</title>
     <link rel="stylesheet" href="{{asset('assets/css/checkout.css')}}">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.7.2/css/all.min.css"
           integrity="sha512-Evv84Mr4kqVGRNSgIGL/F/aIDqQb7xQ2vcrdIwxfjThSH8CSR7PBEakCr51Ck+w+/U6swU2Im1vVX0SVk9ABhg=="
