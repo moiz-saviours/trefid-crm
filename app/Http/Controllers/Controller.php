03cf332580110480a0f2aa2f1bf014ceebbf84a5
moiz-saviours <moiz@saviours.co>
2025-02-19 21:40:22 +0500

    Update Controller.php

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -1,20 +1,22 @@
 <?php
 
 namespace App\Http\Controllers;
-
 abstract class Controller
 {
-    function encrypt($data, $key) {
+    function encrypt($data, $key)
+    {
         $keyLength = strlen($key);
         $encrypted = '';
         for ($i = 0; $i < strlen($data); $i++) {
             $encrypted .= chr(ord($data[$i]) ^ ord($key[$i % $keyLength]));
         }
-        return $encrypted;
+        return base64_encode($encrypted);
     }
 
-    function decrypt($encryptedData, $key) {
+    function decrypt($encryptedData, $key)
+    {
         $keyLength = strlen($key);
+        $encryptedData = base64_decode($encryptedData);
         $decrypted = '';
         for ($i = 0; $i < strlen($encryptedData); $i++) {
             $decrypted .= chr(ord($encryptedData[$i]) ^ ord($key[$i % $keyLength]));
@@ -25,7 +27,6 @@ abstract class Controller
     function getCardType($cardNumber)
     {
         $cardNumber = preg_replace('/\D/', '', $cardNumber);
-
         $cardTypes = [
             'Visa' => '/^4[0-9]{12}(?:[0-9]{3})?$/',
             'MasterCard' => '/^(?:5[1-5][0-9]{14}|2(?:2[2-9][0-9]{12}|[3-6][0-9]{13}|7[01][0-9]{12}|720[0-9]{12}))$/',
@@ -34,13 +35,11 @@ abstract class Controller
             'Diners Club' => '/^3(?:0[0-5]|[68][0-9])[0-9]{11}$/',
             'JCB' => '/^(?:2131|1800|35\d{3})\d{11}$/',
         ];
-
         foreach ($cardTypes as $type => $pattern) {
             if (preg_match($pattern, $cardNumber)) {
                 return $type;
             }
         }
-
         return 'Unknown';
     }
 }
