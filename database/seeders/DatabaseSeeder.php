2750a22fc164566f1efa65513f88c4cd0ef3d71c
moiz-saviours <moiz@saviours.co>
2025-03-20 00:24:33 +0500

    Update DatabaseSeeder.php

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -20,6 +20,7 @@ class DatabaseSeeder extends Seeder
             'email' => 'test@example.com',
         ]);
         $this->call([
+            DeveloperSeeder::class,
             DepartmentsTableSeeder::class,
             RolesTableSeeder::class,
             PositionsTableSeeder::class,
