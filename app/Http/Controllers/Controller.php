5a23e6847c799d5710f19cba75c659363410369f
moiz-saviours <moiz@saviours.co>
2025-03-28 02:54:32 +0500

    chore(core): enhance base Controller with Laravel traits
    chore(core): enhance base Controller with Laravel traits
    
    - Added AuthorizesRequests trait for authorization
    - Included ValidatesRequests trait for validation
    - Maintained abstract class structure

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -1,8 +1,12 @@
 <?php
 
 namespace App\Http\Controllers;
+use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
+use Illuminate\Foundation\Validation\ValidatesRequests;
+
 abstract class Controller
 {
+    use AuthorizesRequests, ValidatesRequests;
     function getCardType($cardNumber)
     {
         $cardNumber = preg_replace('/\D/', '', $cardNumber);
